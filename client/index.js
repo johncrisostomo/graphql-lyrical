581dc3f8adc51066867b0f8714e0d95f85fcb2dd
John Dominic Crisostomo <john.dominic@aktify.com>
2017-08-07 23:19:40 +0800

    integrates react-router

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import SongList from './components/SongList';
@@ -8,9 +9,13 @@ const client = new ApolloClient({});
 
 const Root = () => {
   return (
-    <ApolloProvider client={client}>
-      <SongList />
-    </ApolloProvider>
+    <Router>
+      <ApolloProvider client={client}>
+        <div>
+          <Route path="/" component={SongList} />
+        </div>
+      </ApolloProvider>
+    </Router>
   );
 };
 
